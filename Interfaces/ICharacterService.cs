394906ce2a40f22cf744989f4fc40eab4c0a7883
chani cohen <yaelc5570@gmail.com>
2026-03-15 17:20:49 +0200

    add characterInterface

---
+++ Interfaces/ICharacterService.cs
@@ -0,0 +1,13 @@
+﻿using c__repository_2026.c__service_2026.Dto;
+using Service.DTOs;
+using System.Collections.Generic;
+
+namespace Service.Interfaces
+{
+    internal interface ICharacterService : IService<CharacterDto>
+    {
+        List<CharacterDto> SearchByName(string name);
+        List<CharacterDto> GetTopDetected(int topCount);
+        Dictionary<string, object> GetCharacterStatistics(int characterId);
+    }
+}
