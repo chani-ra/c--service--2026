26be115cb4a56148e5f917bf030ccc588f9b184b
chani ra <035701204a@gmail.com>
2026-03-15 17:22:46 +0200

    did some changes in detectedCharacterDto!!!! חני ויהודית מה נשמע? אני יושבת עכשיו על הפרויקט ... יש לי כמה בעיות עם ההתקנות ש-ד' יעזור!! מה איתכם? עובדות גם? חני הבנתי שאת על הסרביס ויהודית את על ה אי-סרביס..איך הולך? מחכה כבר לעשות pull ולהגיע לתמונה השלמה!! אז הצלחות!!!

--- Dto/DetectedCharacterDto.cs
+++ Dto/DetectedCharacterDto.cs
@@ -8,11 +8,26 @@ namespace c__repository_2026.c__service_2026.Dto
 {
     public class DetectedCharacterDto
     {
-        public int Id { get; set; }
-        public int ImageId { get; set; }
-        public int CharacterId { get; set; }
-        public string CharacterName { get; set; }  // ✨ שם הדמות (לא צריך להחזיר את כל האובייקט)
-        public float Confidence { get; set; }       // ערך בין 0 ל-1 (דיוק הזיהוי)
-        public DateTime DetectionDate { get; set; }
+    
+            
+            public int Id { get; set; }
+
+            
+            public int ImageId { get; set; }
+
+            public int CharacterId { get; set; }
+
+           
+            public string CharacterName { get; set; }
+
+            public double Confidence { get; set; }
+
+            public string FaceCoordinates { get; set; }
+
+            public DateTime DetectionDate { get; set; }
+
+            
+            public string ModelUsed { get; set; }
+        }
     }
-}
+
