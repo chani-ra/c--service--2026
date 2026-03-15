635874d6ba8ce3d9bc7b2a78d7e9852b11937a05
chani cohen <yaelc5570@gmail.com>
2026-03-15 14:20:36 +0200

    addIService

---
+++ Interfaces/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceRecognition.Service.Interfaces
+{
+    public interface IService<T>
+    {
+        Task<List<T>> GetAll();
+        Task<T> Get(int id);
+        Task<T> AddItem(T item);
+        Task<bool> UpdateItem(int id, T item);
+        Task<bool> DeleteItem(int id);
+    }
+}
